b840cfc029a6e20c10decf6b6aebc80174c0fa97
José <jose@MacBook-Pro-de-Jose-2.local>
2021-04-02 01:02:39 +0200

    conditional route home in menu.jsx

--- src/authentication-firebase/Menu.jsx
+++ src/authentication-firebase/Menu.jsx
@@ -1,17 +1,12 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useContext } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { auth } from "./firebaseconfig";
 import { UserContext } from "../UserContext.js";
 
-
 const Menu = () => {
-
-  const {
-    user, setUser
-  } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
 
   const history = useHistory();
-  /* const [user, setUser] = useState(null); */
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
@@ -30,14 +25,18 @@ const Menu = () => {
   return (
     <>
       <nav className="navbar navbar-expand-md navbar-dark bg-dark d-flex justify-content-around p-3">
-      <h1>F r e e   N u t r i t i o n  P l a n n e r  .  O R G</h1>
+        <h1>F r e e N u t r i t i o n P l a n n e r . O R G</h1>
         <ul className="navbar-nav mr-auto ">
-          
           <li className="nav-item">
-            <Link className="nav-link" to="/">
-              Home
-            </Link>
+            {user === "admin@admin.com" ? (
+              <Link className="nav-link" to="/admin">
+                Home
+              </Link>
+            ) : (
+              <span></span>
+            )}
           </li>
+
           <li className="nav-item">
             {!user ? (
               <Link className="nav-link" to="/login">
@@ -48,7 +47,7 @@ const Menu = () => {
             )}
           </li>
           <li className="nav-item">
-            {user ==="admin@admin.com" ? (
+            {user === "admin@admin.com" ? (
               <Link className="nav-link" to="/admin">
                 Admin
               </Link>
@@ -65,10 +64,6 @@ const Menu = () => {
         ) : (
           <span></span>
         )}
-
-
-
-
       </nav>
     </>
   );
