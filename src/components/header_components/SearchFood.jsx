89fc559964a6f5ef8b2294b7cd94fe50c0a6be74
José <jose@MacBook-Pro-de-Jose-2.local>
2021-04-09 22:26:42 +0200

    fix error message login

--- src/components/header_components/SearchFood.jsx
+++ src/components/header_components/SearchFood.jsx
@@ -780,7 +780,7 @@ const SearchFood = () => {
             </div>
           </div>
 
-          <div className="col mt-5">
+          <div className="col mt-3">
             <button
               className=" btn btn-success"
               type="button"
